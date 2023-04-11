99ca4e8b767cf5a74670c11c4dad2ea3bddfc59c
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-04-11 14:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-//complete this code
 class Animal {
-	constructor(species){
-		this.species = species;
-	}
-	get species(){
-	 return this.species;
-	}
-	makeSound(){
-		console.log("The animal makes a sound.");
-	}
+  constructor(species) {
+    this._species = species;
+  }
+  get species() {
+    return this._species;
+  }
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
 }
-
-class Dog extends Animal {
-	bark(){
-		consolee.log("woof");
-	}
-}
-
 class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
+  purr() {
+    console.log("purr");
+  }
 }
-
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+function getElementsByAttribute(attribute, value) {
+  const elementsWithAttribute = [];
+  const allElements = document.getElementsByTagName("*");
+
+  for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].getAttribute(attribute) === value) {
+      elementsWithAttribute.push(allElements[i]);
+    }
+  }
+
+  return elementsWithAttribute;
+}
+ const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+    console.log(elementsWithBarBaz);
